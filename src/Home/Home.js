37c5234f20952c3c40b769d11d54109b74e9847d
unknown <abdulkaderjilani120@gmail.com>
2022-08-12 16:52:27 +0600

    form input add

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,27 +1,60 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 import './Home.css'
 const Home = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+       
+    };
     return (
-        <div>
-            <div className='add-data'>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Add
-                </button>
+        <div className='add-main'>
+            <div className="container">
+                <div className='add-data'>
+                    <div className='d-flex justify-content-end'>
+                        <button type="button" className="modal-submit-btn btn btn-primary px-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Add
+                        </button>
+                    </div>
 
 
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Add Data</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Add Data</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div class="modal-body">
+
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                                            <input className='form-control' type="name" {...register("name", { required: true })} />
+                                            <p className='text-danger'>{errors.name?.type === 'required' && "Name is required"}</p>  
+                                         </div>                                        
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Phone Number</label>
+                                            <input className='form-control' type="text" {...register("number", { required: true })} />
+                                            <p className='text-danger'>{errors.number?.type === 'required' && "Phone Number is required"}</p>  
+                                         </div>
+                                         <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Email Address</label>
+                                            <input className='form-control' type="email" {...register("email", { required: true })} />
+                                            <p className='text-danger'>{errors.email?.type === 'required' && "Email is required"}</p>  
+                                         </div>
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Your Hobbies</label>
+                                             <textarea className='form-control' type="text" {...register("hobbies", { required: true })} cols="30" rows="4">
+                                             </textarea>
+                                           
+                                            <p className='text-danger'>{errors.hobbies?.type === 'required' && "Hobbies is required"}</p>  
+                                         </div>                                        
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                        <input type="submit" class="btn btn-warning px-3 text-white fw-bold"  value="Save Now" />
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
